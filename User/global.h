85c960508a0d6260ea57255edebec00950939007
TarKhnarb <56969200+TarKhnarb@users.noreply.github.com>
2022-05-11 15:45:48 +0200

    Variables
    Variables en adéquation avec les autres commits

--- User/global.h
+++ User/global.h
@@ -6,8 +6,11 @@
 
 #include "constantes.h"
 
+//__________ affichagelcd.h __________
 extern char chaine[30]; // buffer pour l'affichage sur le LCD
 extern uint16_t touch_x, touch_y ;
+
+//__________ menu.h __________
 extern unsigned char numMenu;
 
 //__________ bouton.h __________
@@ -17,10 +20,16 @@ extern bool maintienBouton2;
 extern bool oldMaintienBouton;
 extern bool oldMaintienBouton2;
 
-//__________ CAN.h __________
+//__________ capteurs.h __________
 extern uint16_t temperature;
+extern uint16_t luminosite;
+extern uint16_t co2;
+extern uint16_t humidite;
 
-
-//__________ emetteur.h __________
+//__________ bluetooth.h __________
 extern bool flagBluetooth;
 extern bool flagTIM1;
+extern char stringToSend[7];
+
+//__________ application.h __________
+extern uint8_t sensorToUpdate;
