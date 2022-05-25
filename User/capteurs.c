9be1e25a18e1557ada48f993357233816a915a1e
pasdinspi2 <99338047+pasdinspi2@users.noreply.github.com>
2022-05-25 15:21:41 +0200

    capteur température
    récupération d'une valeur non conforme à la température réelle

--- User/capteurs.c
+++ User/capteurs.c
@@ -3,16 +3,17 @@
 void initTempSensor(void){
 	
 		// P1.31:
+	
 	PINSEL_CFG_Type			configPin; 
-
-	configPin.Funcnum 	 = 2;
+	PCONP_ |= (1 << 12); //AD0 
+	configPin.Funcnum 	 = 3;
 	configPin.OpenDrain = PINSEL_PINMODE_NORMAL;
 	configPin.Pinmode 	 = PINSEL_PINMODE_NORMAL; //output
-	configPin.Portnum 	 = 0;
+	configPin.Portnum 	 = 1;
 	configPin.Pinnum 	 = 31;
 	PINSEL_ConfigPin(&configPin);
 	
-	PCONP_ |= (1 << 12); //AD0 
+	
 	
 	ADC_Init(LPC_ADC, 100000);
 	ADC_ChannelCmd (LPC_ADC, 5, ENABLE);
@@ -21,13 +22,13 @@ void initTempSensor(void){
 void getTempSensor(void){
 	
 	uint16_t tmp;
-	float a;
+	float a=0.f;
 	ADC_StartCmd(LPC_ADC, ADC_START_NOW);
 	while(!ADC_ChannelGetStatus(LPC_ADC, 5, 1)){}
 	
-	temperature = ADC_ChannelGetData(LPC_ADC, 5);
-		/*
-	a = (float)(1023-tmp)*10000/tmp;
-	temperature = 1/(log(a/10000)/(3975)+1/298.15)-273.15;
-		*/
+	tmp = ADC_ChannelGetData(LPC_ADC, 5);
+		
+	a += abs((float)((1023-tmp)*10000/tmp));
+	temperature = 1.f/(log(a/10000.f)/3975.f+1.f/298.15f)-273.15f;
+		
 }
